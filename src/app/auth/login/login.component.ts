6ee31bbd3450c45e542dacb022972e909a4dd2ab
daniela2807 <dany_julio28@hotmail.com>
2020-06-15 22:52:16 -0500

    Final1

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,7 @@
 import { FirestoreService } from './../../services/firestore/firestore.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 
@@ -14,38 +14,48 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   loginForm = new FormGroup({
-    Correo : new FormControl(''),
+    Correo: new FormControl(''),
     Contraseña: new FormControl(''),
   });
-  
+
 
   constructor(private authSvc: FirestoreService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  async onLogin(){
-    const {Correo, Contraseña} = this.loginForm.value;
+  async onLogin() {
+    const { Correo, Contraseña } = this.loginForm.value;
     //console.log(this.loginForm.value);
-    try{
+    try {
       const user = await this.authSvc.login(Correo, Contraseña);
-      console.log(user)
-      if(Correo === 'admin@hotmail.com' && user){
+      //console.log(user)
+      if (Correo === 'admin@hotmail.com' && user) {
         this.router.navigate(['/admin']);
       }
-      else if(user){
+      else if (user) {
         //redirect to homepage
         this.router.navigate(['/home']);
       }
-      else{
+      else {
         document.getElementById('Comprobar').innerText = "Cuenta no encontrada, verifica";
       }
     }
-    catch(error){
+    catch (error) {
       console.log(error);
     }
     this.authSvc.login(Correo, Contraseña);
     //console.log("Form->", this.loginForm.value);
+  }
+
+  Restablecer(correo: string) {
+    console.log(correo);
+    this.authSvc.afAuth.sendPasswordResetEmail(correo).then(function(){
+      console.log("email enviado a"+correo);
+      //email enviado
+    }).catch(function(error){
+
+    });
   }
 
 }
