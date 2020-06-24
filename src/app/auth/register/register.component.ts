f23850f8d7630ae2a0ef9239db5d61ab465e1897
daniela2807 <dany_julio28@hotmail.com>
2020-06-24 18:56:56 -0500

    final

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FirestoreService } from '../../services/firestore/firestore.service';
-import { FormGroup, FormControl, Validators , FormBuilder} from '@angular/forms';
-import { FormsModule} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -12,7 +12,8 @@ import { Observable } from 'rxjs';
   providers: [FirestoreService],
 })
 export class RegisterComponent implements OnInit {
-
+  public contra1 = document.getElementById("contra1");
+  public contra2 = document.getElementById("contra2");
   public user$: Observable<any> = this.firestoreservice.afAuth.user;
   public newClientForm = new FormGroup({
     Nombre: new FormControl('', Validators.required),
@@ -24,10 +25,12 @@ export class RegisterComponent implements OnInit {
     id: new FormControl('')
   });
 
-  constructor(private firestoreservice: FirestoreService, private router: Router) { }
+  constructor(private firestoreservice: FirestoreService, private router: Router) {
+  }
 
   ngOnInit(): void {
   }
+
   public newClient(form) {
     let activo = true;
     let f = new Date();
@@ -43,28 +46,36 @@ export class RegisterComponent implements OnInit {
     }
     let data2 = {
       Correo: form.Correo,
-      Contraseña : form.Contraseña
+      Contraseña: form.Contraseña
     }
-    this.firestoreservice.createClient(data).then(() => {
-      console.log('Documento creado exitosamente');
-      this.newClientForm.setValue({
-        Nombre: '',
-        Apellido: '',
-        Edad: '',
-        Correo: '',
-        Contraseña: '',
-        Contraseña2: '',
-        id: '',
+    if (form.Contraseña.length < 5) {
+      document.getElementById("nomatch").innerText = "Contraseña muy corta";
+    }
+    else if(form.Contraseña !== form.Contraseña2) {
+      document.getElementById("nomatch").innerText = "Contraseñas no coinciden";
+    }
+    else {
+      this.firestoreservice.createClient(data).then(() => {
+        console.log('Documento creado exitosamente');
+        this.newClientForm.setValue({
+          Nombre: '',
+          Apellido: '',
+          Edad: '',
+          Correo: '',
+          Contraseña: '',
+          Contraseña2: '',
+          id: '',
+        });
+      }, (error) => {
+        console.error(error);
       });
-    }, (error) => {
-      console.error(error);
-    });
 
-    console.log(data2.Correo, data2.Contraseña);
-    const user = this.firestoreservice.register(data2.Correo, data2.Contraseña);
-    console.log(user);
-    if(user){
-      this.router.navigate(['/home']);
+      console.log(data2.Correo, data2.Contraseña);
+      const user = this.firestoreservice.register(data2.Correo, data2.Contraseña);
+      console.log(user);
+      if (user) {
+        this.router.navigate(['/home']);
+      }
     }
   }
 
